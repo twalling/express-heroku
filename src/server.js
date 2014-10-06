96a021da5ee9a72d6e6534747de603f68838a75a
Tim Walling <mail@timwalling.com>
2014-10-06 12:29:57 -0400

    updated with express 4.x

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var morgan = require('morgan');
 
-var app = express(express.logger());
+var app = express();
+app.use(morgan('combined'));
 
 app.get('/', function(req, res) {
   res.send('Express Heroku');
